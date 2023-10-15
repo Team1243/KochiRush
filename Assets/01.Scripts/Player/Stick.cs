4bc5580338d5151a808c8ae491645dd8d6e6c191
Yoon HeeSoo <improveme7@gmail.com>
2023-10-16 00:29:24 +0900

    show stick with fruits

--- Assets/01.Scripts/Player/Stick.cs
+++ Assets/01.Scripts/Player/Stick.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
-    private List<CircleObj> circleList;
-    public List<CircleObj> CircleList => circleList;
+    private List<Fruit> fruitList;
 
-    public void SetFruitList()
+    private Transform fruitBasketTr;
+
+    private Vector3 startPos;
+
+    private void Awake()
     {
+        fruitBasketTr = transform.GetChild(1).GetComponent<Transform>();
+    }
 
+    public void SetFruitList(List<Fruit> tempList)
+    {
+        fruitList = tempList;
+        StartCoroutine(ShowStick());
+    }
+
+    private IEnumerator ShowStick()
+    {
+        // startPos = fruitBasketTr.position;
+        Vector3 pos = fruitBasketTr.position;
+        for (int i = 0; i < fruitList.Count; i++)
+        {
+            fruitList[i].transform.position = pos;
+
+            // Â¦¼öÀÏ ¶§
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    pos += Vector3.one;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    pos -= Vector3.one;
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     public void RemoveFruitList()
     {
-
+        fruitList.Clear();
     }
 }
--- Assets/01.Scripts/Player/StickCollision.cs
+++ Assets/01.Scripts/Player/StickCollision.cs
@@ -1,19 +1,23 @@
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 
 public class StickCollision : MonoBehaviour
 {
     private StickMovement _stickMovement;
+    private Stick _stick;
 
+    // ray
     private LayerMask whatIsFruit;
-
     private Transform fruitBasketTr;
     private BoxCollider2D fruitBasketCol;
+
+    private List<Fruit> fruitListTemp = new List<Fruit>(5);
 
     private void Awake()
     {
         _stickMovement = GetComponent<StickMovement>();
+        _stick = GetComponent<Stick>();
+
         fruitBasketTr = transform.GetChild(1).GetComponent<Transform>();
         fruitBasketCol = fruitBasketTr.GetComponent<BoxCollider2D>();
 
@@ -27,16 +31,23 @@ public class StickCollision : MonoBehaviour
 
     private void CheckCollisionObj()
     {
-        Debug.Log("move finish");
         Debug.Log("check collision start");
 
         RaycastHit2D[] hits;
         hits = Physics2D.BoxCastAll(fruitBasketTr.position, fruitBasketCol.size, 0f, fruitBasketTr.up, 0f, whatIsFruit);
 
+        fruitListTemp.Clear();
         foreach (var hit in hits)
         {
-            Debug.Log(hit.collider.gameObject.name);
+            if (hit.transform.TryGetComponent(out Fruit fruit))
+            {
+                if (fruitListTemp.Count < fruitListTemp.Capacity)
+                {
+                    fruitListTemp.Add(fruit);
+                }
+            }
         }
+        _stick.SetFruitList(fruitListTemp);
 
         Debug.Log("check collision end");
     }
