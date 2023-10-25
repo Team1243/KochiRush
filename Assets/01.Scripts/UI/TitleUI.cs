90ceb95a7cbd7826ad52c8068954a4960b011d28
Dongyeop <ddongyeop0129@gmail.com>
2023-10-25 19:33:05 +0900

    Main UI

--- Assets/01.Scripts/UI/TitleUI.cs
+++ Assets/01.Scripts/UI/TitleUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -7,12 +9,18 @@ public class TitleUI : MonoBehaviour
 {
     private UIDocument _uiDocument;
     private VisualElement _rootElement;
+    private VisualElement _pausePanel;
+    private VisualElement _buttonContainer;
+    private VisualElement _hand;
     private Button _startButton;
+    private Button _pauseButton;
     private Button _soundButton;
     private Button _exitButton;
 
     [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private List<Sprite> _audioSprite;
+
+    private bool _isPause;
     
     private void Awake()
     {
@@ -23,8 +31,13 @@ public class TitleUI : MonoBehaviour
 
     private void OnEnable()
     {
+        _isPause = false;
         _rootElement = _uiDocument.rootVisualElement;
+        _pausePanel = _rootElement.Q<VisualElement>("PausePanel");
+        _buttonContainer = _rootElement.Q<VisualElement>("Buttons");
+        _hand = _rootElement.Q<VisualElement>("Hand");
         _startButton = _rootElement.Q<Button>("Container");
+        _pauseButton = _rootElement.Q<Button>("PauseButton");
         _soundButton = _rootElement.Q<Button>("SoundButton");
         _exitButton = _rootElement.Q<Button>("ExitButton");
 
@@ -34,8 +47,16 @@ public class TitleUI : MonoBehaviour
             _soundButton.style.backgroundImage = new StyleBackground(_audioSprite[1]);
         
         _startButton.RegisterCallback<ClickEvent>(e => { e.StopPropagation(); UIManager.Instance.GameStart(); });
+        _pauseButton.RegisterCallback<ClickEvent>(PauseButton);
         _soundButton.RegisterCallback<ClickEvent>(e => { e.StopPropagation(); SoundBtnClick(); });
         _exitButton.RegisterCallback<ClickEvent>(e => { e.StopPropagation(); Application.Quit(); });
+
+        StartCoroutine(HandCo());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
     private void SoundBtnClick()
@@ -51,6 +72,34 @@ public class TitleUI : MonoBehaviour
             _audioMixer.SetFloat("Master", 0); //Å°´Â°Å
             _soundButton.style.backgroundImage = new StyleBackground(_audioSprite[0]);
             PlayerPrefs.SetInt("Audio", 1);
+        }
+    }
+
+    private void PauseButton(ClickEvent evt)
+    {
+        evt.StopPropagation();
+        
+        if (_isPause)
+        {
+            _pausePanel.RemoveFromClassList("show");
+            _buttonContainer.RemoveFromClassList("show");
+        }
+        else
+        {
+            _pausePanel.AddToClassList("show");
+            _buttonContainer.AddToClassList("show");
+        }
+        _isPause = !_isPause;
+    }
+
+    private IEnumerator HandCo()
+    {
+        while (true)
+        {
+            _hand.AddToClassList("small");
+            yield return new WaitForSeconds(0.5f);
+            _hand.RemoveFromClassList("small");
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
