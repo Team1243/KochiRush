e3c8492adc711df075f7b6312f0c997dda9faded
Dongyeop <ddongyeop0129@gmail.com>
2023-12-04 22:30:20 +0900

    Last Build

--- Assets/01.Scripts/UI/LosetUI.cs
+++ Assets/01.Scripts/UI/LosetUI.cs
@@ -10,8 +10,6 @@ public class LosetUI : MonoBehaviour
     private UIDocument _uiDocument;
     private VisualElement _rootElement;
     private Button _backButton;
-    private VisualElement _admob;
-    private Button _admobButton;
     private Label _scoreLabel;
     private Stick _stick;
     
@@ -28,42 +26,12 @@ public class LosetUI : MonoBehaviour
     {
         _rootElement = _uiDocument.rootVisualElement;
         _backButton = _rootElement.Q<Button>("Container");
-        _admob = _rootElement.Q<VisualElement>("Admob");
-        _admobButton = _admob.Q<Button>("AdmobButton");
         _scoreLabel = _rootElement.Q<Label>("ScoreText");
         _backButton.RegisterCallback<ClickEvent>(e =>
         {
             e.StopPropagation();
             UIManager.Instance.GameReStart();
         });
-        _admobButton.RegisterCallback<ClickEvent>(e =>
-        {
-            e.StopPropagation();
-
-            // 광고 재생
-            SceneManager.LoadScene(0);
-            RewardedAdManager.Instance.ShowAd();
-        });
         _scoreLabel.text = _stick.CurrentScore.ToString();
-        
-        // Admob << 키고 끄기
-        StartCoroutine(AdmobButtonSmallCo());
-    }
-    
-    //작아지는거 해야댐 코루틴 
-    private IEnumerator AdmobButtonSmallCo()
-    {
-        _currentTime = 0;
-        
-        
-        while (_currentTime < _admobButtonEnableTime)
-        {
-            yield return null;
-            _currentTime += Time.deltaTime;
-            float time = _currentTime / _admobButtonEnableTime;
-            _admobButton.style.backgroundSize = new StyleBackgroundSize(new BackgroundSize(Length.Percent(Mathf.Lerp(60, 0, time)), Length.Percent(Mathf.Lerp(60, 0, time)))); 
-        }
-
-        _admobButton.visible = false;
     }
 }
